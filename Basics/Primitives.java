fbd77f68aeab19fdb577ece3278264e6677e59b5
siddheshpotdar2003 <potdarsiddhesh2003@gmail.com>
2025-01-05 04:54:36 +0000

    Primitives

---
+++ Basics/Primitives.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+        // int i = 123;
+        // long l = 78945612365L;
+        // float f = 2.34f;
+        // float f = 251866.515464f;
+        // double d = 251866.515464; 
+        // System.out.println(d);
+        
+        // int tenMillion = 10,000,000;
+        int tenMillion = 10_000_000;
+        System.out.println(tenMillion);
+    }
+}
