52342a4c454da4fcc89e2c2c88fda1e5d4691880
siddheshpotdar2003 <potdarsiddhesh2003@gmail.com>
2025-01-05 06:49:30 +0000

    Type Casting & Conversion

---
+++ Basics/TypeCasting.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        
+        // float a = 10f;
+        // int a = 3/2;
+        // System.out.println(a);
+
+        // char c = 'a';
+        // int c = 'a';
+        // System.out.println(c);
+
+        // float f = 23.89f;
+        // int a = (int)f;
+        // System.out.println(a);
+
+        int i = 3;
+        float f = 0.5f;
+        double result = i * f;
+        System.out.println(result);        
+    }
+}
